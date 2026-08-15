8f44459985d98633a029ef00ee3c11b60c22bfaf
Seidy Alanis <seidyalanis@gmail.com>
2026-08-14 22:57:08 -0600

    Establece la ruta para el fronted.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
