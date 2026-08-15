8f25a90cefa5269ea7f1f27b57d47d758a3e34fe
Seidy Alanis <seidyalanis@gmail.com>
2026-08-15 00:11:40 -0600

    Incluye ruta para eventos.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require('./routes/eventRoutes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
 
 // Manejador simple de rutas no encontradas
 app.use((req, res) => {
