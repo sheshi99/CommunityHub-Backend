8b27a09a7ef99576d08771af41bca83cb2ddce18
Seidy Alanis <seidyalanis@gmail.com>
2026-08-15 00:18:50 -0600

    Incluye ruta de categorias

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const eventRoutes = require('./routes/eventRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
+app.use('/api/categories', categoryRoutes);
 
 // Manejador simple de rutas no encontradas
 app.use((req, res) => {
